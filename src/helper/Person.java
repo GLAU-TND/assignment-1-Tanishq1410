5e22644c8af327c5f00fd185eee8fd15e39b4e59
Shriyanshi1104 <shriyanshi.baranwal_cs18@gla.ac.in>
2020-02-24 20:23:03 +0530

    Getter and Setter method for email address and fetching and inserting phone number are created

--- src/helper/Person.java
+++ src/helper/Person.java
@@ -28,4 +28,30 @@ public class Person implements Comparable<Person> {
         System.out.println(lastName);
         this.lastName = lastName;
     }
+
+    //Getter for Email ID
+    public String getEmailID() {
+        return emailID;
+    }
+
+    //Setter for EmailID
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    //Getter for fetching all phone numbers
+    public StringBuffer getPhoneNumber() {
+        return this.phoneNumbers;
+    }
+
+    //Setter for inserting new phone numbers
+    public void setPhoneNumbers(String phoneNumber) {
+        if (phoneNumbers == null) {
+            this.phoneNumbers = new StringBuffer(phoneNumber);
+        } else {
+            this.phoneNumbers.append(", " + phoneNumber);
+        }
+    }
+
+
 }
