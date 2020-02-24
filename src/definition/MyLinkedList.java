b19c088e01d30a36a5ba021e21d82d56ba7e3093
Shriyanshi1104 <shriyanshi.baranwal_cs18@gla.ac.in>
2020-02-24 20:30:00 +0530

    Methods to delete and insert node form are created

--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -23,4 +23,50 @@ public class MyLinkedList<T extends  Comparable<T>> {
         this.iter = iter;
     }  //Setter for iter
 
+    //method to insert new node
+    public void insert(Node<T> node) {
+        if (getHead() == null) {
+            setHead(node);
+        } else {
+            Node<T> temp = head;
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(node);
+        }
+    }
+
+    //method to delete node from beginning
+    public Node<T> delete() {
+        if (isEmpty()) {
+            System.out.println("Underflow");
+            return null;
+        }
+        Node<T> temp = getHead();
+        setHead(getHead().getNext());
+        return temp;
+    }
+
+    //method to delete node at particular position
+    public Node<T> delete(int i) {
+        Node<T> deletedNode ;
+        Node<T> temp = getHead();
+        if (isEmpty()) {
+            System.out.println("Underflow");
+            return temp;
+        }
+        if (i == 1) {
+            deletedNode = getHead();
+            setHead(null);
+        } else {
+            for (int j = 1; j < i - 1; j++) {
+                temp = temp.getNext();
+            }
+            deletedNode = temp.getNext();
+            temp.setNext(temp.getNext().getNext());
+        }
+        return deletedNode;
+    }
+
+
 }
