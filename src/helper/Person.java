1a4dc38b9dd5d623231ee2ba3fca978b2cc33e1c
Tanish1410 <shriyanshi.baranwal_cs18@gla.ac.in>
2020-02-24 22:00:06 +0530

    getter for fetching all phone number and setter for inserting new phone numbers are craeted

--- src/helper/Person.java
+++ src/helper/Person.java
@@ -29,4 +29,28 @@ public class Person implements Comparable<Person> {
         this.lastName = lastName;
     }
 
+    //Getter for Email ID
+    public String getEmailID() {
+        return emailID;
+    }
+
+    //Setter for EmailID
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    //Getter for fetching all phone numbers
+    public StringBuffer getPhoneNumber() {
+        return this.phoneNumbers;
+    }
+
+    //Setter for inserting new phone numbers
+    public void setPhoneNumbers(String phoneNumber) {
+        if (phoneNumbers == null) {
+            this.phoneNumbers = new StringBuffer(phoneNumber);
+        } else {
+            this.phoneNumbers.append(", " + phoneNumber);
+        }
+    }
+
 }
