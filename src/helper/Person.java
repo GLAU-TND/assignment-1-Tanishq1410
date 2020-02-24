28625ee3ffa0e9397b760bf750cfbe801d7b86f2
Shriyanshi1104 <shriyanshi.baranwal_cs18@gla.ac.in>
2020-02-24 20:21:39 +0530

    Getter and Setter method for first and last name are ready

---
+++ src/helper/Person.java
@@ -0,0 +1,31 @@
+package helper;
+
+public class Person implements Comparable<Person> {
+
+    private String firstName;                   //First name of person
+    private String lastName;                    //Last name of person
+    private String emailID;                     //EmailId of person
+    private StringBuffer phoneNumbers;          //Phone numbers of person
+
+    //Getter for first name
+    public String getFirstName() {
+        return firstName;
+    }
+
+    //Setter for first name
+    public void setFirstName(String firstName) {
+        System.out.println(firstName);
+        this.firstName = firstName;
+    }
+
+    //Getter for last name
+    public String getLastName() {
+        return lastName;
+    }
+
+    //Setter for last name
+    public void setLastName(String lastName) {
+        System.out.println(lastName);
+        this.lastName = lastName;
+    }
+}
